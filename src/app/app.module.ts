bfdafe686b17c0bae4e6c0d20e4846ff539c09f5
Vivek <vivekmjude@gmail.com>
2018-01-23 20:20:49 +0530

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,22 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AngularFireModule } from 'angularfire2';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { FIREBASE_CONFIG } from './firebase.credentials'
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
   ],
   providers: [
     StatusBar,
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDYXLQBR1TcglfyE4PUAR9AiMEx9r_Ep1c",
+  authDomain: "learning-2b147.firebaseapp.com",
+  databaseURL: "https://learning-2b147.firebaseio.com",
+  projectId: "learning-2b147",
+  storageBucket: "",
+  messagingSenderId: "274006603270"
+};
